24334aaaf5b0cf72af74bf530949c4de9d4f72c1
MarkJames0 <2208766148@qq.com>
2020-01-12 17:56:21 +0800

    1/12

---
+++ Day_1/annotation.py
@@ -0,0 +1,16 @@
+"""
+
+第一个Python程序 - hello, world!
+向伟大的Dennis M. Ritchie先生致敬
+
+Version: 0.1
+Author: 骆昊
+Date: 2018-02-26
+
+"""
+
+print('hello, world!')
+# print("你好,世界！")
+print('你好', '世界')
+print('hello', 'world', sep=', ', end='!')
+print('goodbye, world', end='!\n')
---
+++ Day_1/practice_2.py
@@ -0,0 +1,29 @@
+import turtle
+
+"""
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
+"""
+
+turtle.pensize(3)
+turtle.pencolor('pink')
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+# turtle.mainloop()
